70bd7a6e1f0f7e6f8896fc24e07570e2a5442914
Manohar-1 <mudhapakamanohar@gmail.com>
2023-05-09 15:43:28 +0530

    added buttons

--- js/index.js
+++ js/index.js
@@ -1,19 +1,20 @@
-
-
-var menuBtn = document.getElementsByClassName('menu-btn')
-var mobileMenu = document.getElementsByClassName('mobile-menu')
-
-window.addEventListener('resize',()=>{
-    if(mobileMenu[0].classList.contains('active')){
-        mobileMenu[0].classList.remove('active')
-    }
-})
-
-
-var clickedBtn = ()=>{
-    mobileMenu[0].classList.toggle('active')
+function newPage() {
+  window.onload(
+    "https://drive.google.com/file/d/1XqW2mxc9-fozirMbc6f25IAkx0pQVbc2/view?usp=sharing"
+  );
 }
 
-menuBtn[0].addEventListener('click',clickedBtn)
+var menuBtn = document.getElementsByClassName("menu-btn");
+var mobileMenu = document.getElementsByClassName("mobile-menu");
 
+window.addEventListener("resize", () => {
+  if (mobileMenu[0].classList.contains("active")) {
+    mobileMenu[0].classList.remove("active");
+  }
+});
 
+var clickedBtn = () => {
+  mobileMenu[0].classList.toggle("active");
+};
+
+menuBtn[0].addEventListener("click", clickedBtn);
